9889cada470e908f8ce8ee15c9da0b71e97b68b6
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2022-12-06 21:27:33 +0200

    Correction 4 on task 3 file

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
+# def print_reversed_list_integer(my_list=[]):
+    # for i in reversed(my_list):
+        # print("{:d}".format(i))
+
+def print_reversed_list_integer(my_list):
     for i in reversed(my_list):
-        print("{:d}".format(i))
+        x = "{:d}".format(i)
+        n = x.rstrip('\n')
+        print(n)
