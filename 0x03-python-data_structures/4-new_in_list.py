b86a31d12cc4d74517582cd90e1973489394c34f
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-12-07 02:27:18 +0000

    Correction on task 4 file

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if my_list is None:
-        return
+
     new_list = my_list[:]
+
     if 0 <= idx < len(my_list):
         new_list[idx} = element
-    return new_list
+
+    return (new_list)
