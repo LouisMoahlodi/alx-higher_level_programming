6871b7153a6d080b0da38b333ee6e4a4cadcc93d
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-12-13 13:32:20 +0000

    Correction 2 on task 11 file

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def pow(a,b):
-    powOf = a ** b
-    print(powOf)
+def pow(a, b):
+    powOf = a ** b  # Double astris is used to to find the power of a number
+    return (powOf)
