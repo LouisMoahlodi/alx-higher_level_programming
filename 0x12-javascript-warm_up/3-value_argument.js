9ef3635bac49baedeb8dc069930b41bc90854363
LouisMoahlodi <lebushomoahlodi@gmail.com>
2023-03-13 18:31:24 +0000

    Correction on task 3 file

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-args.forEach((arg, index) => {
-  console.log(`${arg}`);
-});
+let numArgs = 0;
+for (let i = 0; i < args.length; i++) {
+  numArgs++;
+}
+
+if (numArgs >= 1) {
+  console.log(`${args[0]}`);
+} else {
+  console.log('No Argument');
+}
