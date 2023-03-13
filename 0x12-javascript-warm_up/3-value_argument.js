f71df4a58551447fa9b7eaf8a9757760f9d8944a
LouisMoahlodi <lebushomoahlodi@gmail.com>
2023-03-13 18:38:52 +0000

    Correction 2 on task 3 file

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,2 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-let numArgs = 0;
-for (let i = 0; i < args.length; i++) {
-  numArgs++;
-}
-
-if (numArgs >= 1) {
-  console.log(`${args[0]}`);
-} else {
-  console.log('No Argument');
-}
+console.log(process.argv[2] ? process.argv[2] : 'No argument');
