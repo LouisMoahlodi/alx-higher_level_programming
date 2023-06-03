2a7b11826d0f2ec8f57595bf1818e615b822f3e0
LouisMoahlodi <lebushomoahlodi@gmail.com>
2023-06-03 12:13:10 +0000

    petty fix on task 0 file

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-# Create a new matrix of the same size as the input matrix
-	new_matrix = matrix.copy()
+    # Create a new matrix of the same size as the input matrix
+    new_matrix = matrix.copy()
 
 # Compute the square value of each integer in the matrix
-	for i in range(len(matrix)):
-		new_matrix[i] = list(map(lambda x: x ** 2, matrix[i]))
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x ** 2, matrix[i]))
 
-	return new_matrix
-
+    return new_matrix
