c9e4226dc1bae0631c7da5fe9ba6b72a5ec3da7f
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-06-03 21:10:38 +0200

    Correction on task 6 file

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_dict = {}
     sorted_keys = sorted(a_dictionary.keys())
 
     for key in sorted_keys:
-        sorted_dict[key] = a_dictionary[key]
+        value = a_dictionary[key]
+        sorted_dict = f"{key}: {value}"
 
     return sorted_dict 
