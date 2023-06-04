18d53a0b8274b53187686b58376522f7fb99a2c2
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-06-04 17:03:43 +0200

    Added task 9 file

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary
+
+    for element in new_dict:
+        if element == int:
+            element * 2
+    return new_dict
