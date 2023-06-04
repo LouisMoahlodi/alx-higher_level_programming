4a3f18a4d5272a43d048aa884869b6ebf6c3e8e3
LouisMoahlodi <lebushomoahlodi@gmail.com>
2023-06-04 15:34:09 +0000

    petty fix on task 10 file

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,9 +3,10 @@ def best_score(a_dictionary):
     # Check if the dictionary is empty
     if not a_dictionary:
         return None
-    
+
     # Find the key with the max value using the max() function
-    # The key parameter is set to a_dictionary.get, which gets the value associated with each key
+    # The key parameter is set to a_dictionary.get, which gets the value
+    # associated with each key
     max_key = max(a_dictionary, key=a_dictionary.get)
 
     return max_key
