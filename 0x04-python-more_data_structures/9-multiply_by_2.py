1fb536e0891f6f43d9a91bcb4ce71f138e65460e
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-06-04 17:13:25 +0200

    Correction on task 9 file

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = a_dictionary
+    # Initialie an empty dictionary to store the multiplied values
+    new_dict = {}
 
-    for element in new_dict:
-        if element == int:
-            element * 2
+    # Iterate over the key-value pairs in the original dictionary
+    for key, value in a_dictionary.items():
+        # Multiply the value by 2 and assign it to the corresponding key in the new dictionary 
+        new_dict[key] = value * 2
+
     return new_dict
