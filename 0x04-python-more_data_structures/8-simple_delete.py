a85611b688ddba0ad4b41fc7d68454cf908645f1
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-06-04 16:56:08 +0200

    Added comments

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
+    # Check if the key exist  in the dictionary
     if key in a_dictionary:
+        # If the key exist, delete the key-value pair using the 'de;' statement
         del a_dictionary[key]
     return a_dictionary
