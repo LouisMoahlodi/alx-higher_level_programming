575f80633c560ecd1d101a73e5c4c3ffc0a69f35
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-06-15 20:59:55 +0200

    Correction task 1 file

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,9 +3,9 @@ def safe_print_integer(value):
         try: 
             # Prints the value as an int using the "{:d}.format()"
             print("{:d}".format(value))
-            print()
             # Return true to indicate success
             return True 
         except (ValueError, TypeError):
               # Returns flase if an error occurs (value is not an int)
               return False
+        print()
