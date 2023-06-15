67276805fbcc3f0f7ebaa0a7ac9b5f1d1c15edcf
Louis <lebushmoahlodi@gmail.com>
2023-06-15 18:36:06 +0000

    petty fix on task 0 file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-def safe_print_list (my_list=[], x=0):
-    ktrack = 0 # Variable to keep track of the number of succesfully printed elements
+def safe_print_list(my_list=[], x=0):
+    ktrack = 0  # Variable to keep track of the number of succesfully printed elements
 
-    for i in range(x): # Loop through the specified range
+    for i in range(x):  # Loop through the specified range
         try:
-            print(f"{my_list[i]}", end="") #Prints the current element wihtout a new line
-            ktrack += 1 # Increments counter if successfuly printed
+            # Prints the current element wihtout a new line
+            print(f"{my_list[i]}", end="")
+            ktrack += 1  # Increments counter if successfuly printed
         except IndexError:
-            #print("Nope: try again chief")
-            break # Exit the loop if index is out of range
-    print() # Print a new line after printin the element
+            # print("Nope: try again chief")
+            break  # Exit the loop if index is out of range
+    print()  # Print a new line after printin the element
     return ktrack
