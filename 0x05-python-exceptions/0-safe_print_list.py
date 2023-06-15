4cbd971b0270b0ab79383828a817369ef43268fd
Louis <lebushmoahlodi@gmail.com>
2023-06-15 18:39:31 +0000

    petty fix 3

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     # Variable to keep track of the number of succesfully printed elements
-    ktrack = 0  
+    ktrack = 0
 
     for i in range(x):  # Loop through the specified range
         try:
