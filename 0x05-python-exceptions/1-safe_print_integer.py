14a8a48e01d96171ee34dd95548a6234a3fd6c28
Louis <lebushmoahlodi@gmail.com>
2023-06-15 19:00:16 +0000

    Petty fix on task 1 file

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-        try: 
-            # Prints the value as an int using the "{:d}.format()"
-            print("{:d}".format(value))
-            # Return true to indicate success
-            return True 
-        except (ValueError, TypeError):
-              # Returns flase if an error occurs (value is not an int)
-              return False
-        print()
+    try:
+        # Prints the value as an int using the "{:d}.format()"
+        print("{:d}".format(value))
+        # Return true to indicate success
+        return True
+    except (ValueError, TypeError):
+        # Returns flase if an error occurs (value is not an int)
+        return False
+    print()
