2aa9e28a597c094b08604e7264691a548288708d
Louis Moahlodi <lebushomoahlodi@gmail.com>
2023-06-16 20:04:24 +0000

    Added task 0 file

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,7 +3,7 @@
 
 
 def lookup(obj):
-   """
-   Returns list of available attributes and methods using the built-in dir() function
-   """
-   return dir(obj)
+    """
+    Returns list of available attributes and methods using the built-in dir() function
+    """
+    return dir(obj)
