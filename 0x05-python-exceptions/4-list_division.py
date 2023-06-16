eb31686cda5d9cbe3379f1840dc0058a128f7342
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-06-16 12:32:29 +0200

    Correction 6 on task 6 file

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,11 +4,11 @@ def list_division(my_list_1, my_list_2, list_length):
     results = []
     # Iterate over the range specified by list_length
     for i in range(list_length):
+         # Access the current element from my_list_1
+        value_1 = my_list_1[i]
+        # Access the current element from my_list_2
+        value_2 = my_list_2[i]
         try:
-            # Access the current element from my_list_1
-            value_1 = my_list_1[i]
-            # Access the current element from my_list_2
-            value_2 = my_list_2[i]
             div_result = value_1 / value_2
 
         except TypeError:
