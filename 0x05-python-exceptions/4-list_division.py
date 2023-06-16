a9ea87e707aa6b96e6aab01deb1e0bfddc31f98d
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-06-16 12:49:10 +0200

    Correction 10 on task 4 file

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     # Empty list to store the division results
     results = []
     # Iterate over the range specified by list_length
-    for i in range(0, list_length):
+    for i in range(list_length):
         try:
             div_result = my_list_1[i] / my_list_2[i]
 
@@ -14,6 +14,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             # Print message if division by zero occurs
             print("division by 0")
+            div_result = 0
 
         except IndexError:
             print("out of range")
