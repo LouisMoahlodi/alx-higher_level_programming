900d63e1bbddf98d7ebc6f2778f8ebc736c4d88c
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-06-16 22:20:29 +0200

    Documented the module and method

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+""" Function that returns the a sorted list of names in the given object"""
+
+
 def lookup(obj):
-    return dir(obj)
+   """
+   Returns list of available attributes and methods using the built-in dir() function
+   """
+   return dir(obj)
