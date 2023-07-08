e363a77ec70787438c6d9b628e432c405378614d
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-07-08 16:11:43 +0200

    Added task 2 file

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A Function that returns  True if the object is of specified class"""
+
+
+def is_same_class(obj, a_class):
+    """ Get the class of the object using the type functions"""
+    obj_class = type(obj)
+
+    # Compare the the object's class with the specified class
+    if obj_class is a_class:
+        return True
+    else:
+        return False
