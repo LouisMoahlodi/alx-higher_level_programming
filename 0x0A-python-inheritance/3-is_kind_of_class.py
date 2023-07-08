0ef0ce1b5f389cb9f17c6802f51ad9f1e2cd9c76
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-07-08 17:25:32 +0200

    Added task 3 file

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" A Function that checks is instance of a class or an inherited class """
+
+
+
+def is_kind_of_class(obj, a_class):
+    """ Returns the true is instance of the class or the inherited class"""
+    if isinstance(a_class):
+        return True
+    else:
+        return False
+    
