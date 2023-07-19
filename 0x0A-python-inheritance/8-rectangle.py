31baebb8ca98d75c257c6ec7be12eb5e0567487d
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-07-19 20:44:22 +0200

    Added task 8 file

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Empty class"""
+
+class BaseGeometry:
+    """ A function for geometry """
+
+    def area(self):
+        """ Method to calculate the area """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Method to validate an integer value """
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        super().__init__()
+        self.__width = width
+        self.__height = height
+        self.__integer_validator("width", self.__width)
+        self.__integer_validator("height", self.__height)
