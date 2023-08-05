06dbc5768fa37125f9940fa9f9dae0d2f8f2763a
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-08-05 15:53:23 +0200

    Added task 10 file

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Inherits from Rectnagle """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """ 
+    Initializes sqaure instance 
+    Args:
+        size (int): the size of the square using the area  function
+    """ 
+    
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator("size", size)
+        
