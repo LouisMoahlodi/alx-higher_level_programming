016c7854883a819feae029250f846bbaf7206084
Louis Moahlodi <lebushomoahlodi@gmail.com>
2023-08-05 12:49:34 +0000

    petty fix on task 7 file

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,14 +12,14 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """validates a value as an integer
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
-#The first block of code is the correct implementation for the task because it 
-# directly checks if the type of value is exactly int, whereas the second block of code 
-# checks if value is an instance of the int class. 
+# The first block of code is the correct implementation for the task because it
+# directly checks if the type of value is exactly int, whereas the second block of code
+# checks if value is an instance of the int class.
 
 # class BaseGeometry:
 #     """ A function for geometry """
