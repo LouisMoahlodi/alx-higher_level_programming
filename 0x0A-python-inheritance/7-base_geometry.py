be64c662e20c5c1419a95f6800365c529f456753
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-08-05 14:25:40 +0200

    Correction 9 on task 7 file

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
-""" Empty class"""
+"""Defines a base geometry class BaseGeometry"""
 
 
 class BaseGeometry:
-    """ A function for geometry """
+    """this class represents a base geometry"""
 
     def area(self):
-        """ Method to calculate the area """
+        """method not implemented yet"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ Method to validate an integer value """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+        """validates a value as an integer
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+# #!/usr/bin/python3
+# """ Empty class"""
+
+
+# class BaseGeometry:
+#     """ A function for geometry """
+
+#     def area(self):
+#         """ Method to calculate the area """
+#         raise Exception("area() is not implemented")
+
+#     def integer_validator(self, name, value):
+#         """ Method to validate an integer value """
+#         if not isinstance(value, int):
+#             raise TypeError(f"{name} must be an integer")
+#         elif value <= 0:
+#             raise ValueError(f"{name} must be greater than 0")
