b380a915c8a9259e6f42ebb3241948d2ec51a6dd
Louis Moahlodi <lebushomoahlodi@gmail.com>
2023-08-05 13:13:01 +0000

    etty fix on task 8 file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,6 +3,7 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """
     Initializes a Rectangle instance
@@ -10,6 +11,7 @@ class Rectangle(BaseGeometry):
         width (int): The width of the rectangle
         height (int): The height of the rectangle
     """
+
     def __init__(self, width, height):
         """ Initializes a new Rectangle"""
         self.integer_validator("width", width)
