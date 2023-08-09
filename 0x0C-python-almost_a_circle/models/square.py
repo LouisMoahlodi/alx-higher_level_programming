dbefea8056687c628579fd963d65680c9d9a0188
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-08-09 13:53:06 +0200

    Updated square: size method with getter/setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,8 +32,20 @@ class Square(Rectangle):
         self.size = size
         self.x = x
         self.y = y
-        self.id = None  # This seems like a typo. Should it be `self.id = id`?
+        self.id = None
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
+        self.__height = value
 
     def __str__(self):
         """
