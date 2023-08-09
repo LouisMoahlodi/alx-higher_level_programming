78e382059ce6bae2261cbe5fc5b52414b195483d
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-08-09 07:55:45 +0200

    Created the base class and tested.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
+""" Base class created """
 class Base:
- """ A base class for all other classes"""
- __nb_objects = 0
+    """ A base class for all other classes"""
+    __nb_objects = 0
 
-def __init__(self, id=None):
+    def __init__(self, id=None):
         """ Initializes the base instance"""
         if id is not None:
             self.id = id
