fcef3e8cff85097bc57b2ba60af125f7b64c96c5
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-08-09 14:34:38 +0200

    Added dictionaires to sqaure and rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -177,3 +177,14 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+    
+    def to_dictionary(self):
+        """Returns dictionary representation."""
+
+        return{
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x" : self.x,
+            "y" : self.y
+        }
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -85,3 +85,12 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+    def to_dictionary(self):
+        """Returns dictionary representation."""
+
+        return{
+            "id": self.id,
+            "size": self.size,
+            "x" : self.x,
+            "y" : self.y
+        }
