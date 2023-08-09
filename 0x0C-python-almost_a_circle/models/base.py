b0711bb28a81f4493fe3bb71ecec18185f051e0c
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-08-09 07:47:25 +0200

    Added base clase/taks 1 files

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,11 @@
+class Base:
+ """ A base class for all other classes"""
+ __nb_objects = 0
+
+def __init__(self, id=None):
+        """ Initializes the base instance"""
+        if id is not None:
+            self.id = id
+        else: 
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
