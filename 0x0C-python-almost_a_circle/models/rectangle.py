235ccff4c4eef003c0f8e5f93448b57e2107c3c3
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-08-09 10:43:06 +0200

    updated rectangle with update method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -153,3 +153,20 @@ class Rectangle(Base):
             f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "
             f"{self.__width}/{self.__height}"
         )
+    
+    def update(self, *args):
+        """Updates the attributes of this object.
+        
+        Args:
+        *args: Positional arguments is the order id, width, height, x, y
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
