3e623220ee9d5e635082a1d0e38cae9749dcfe6f
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-08-09 13:34:00 +0200

    Added square file

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Module containing the Square class that inherits from Rectangle.
+"""
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """
+    A class representing a square that inherits from Rectangle.
+
+    Attributes:
+        size (int): The size of the square's sides.
+        x (int): The x-coordinate of the square's position.
+        y (int): The y-coordinate of the square's position.
+        id (int): The unique identifier of the square.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square's sides.
+            x (int, optional): The x-coordinate of the square's position. Defaults to 0.
+            y (int, optional): The y-coordinate of the square's position. Defaults to 0.
+            id (int, optional): The unique identifier of the square. Defaults to None.
+        """
+        self.size = size
+        self.x = x
+        self.y = y
+        self.id = None  # This seems like a typo. Should it be `self.id = id`?
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Returns a string representation of the Square instance.
+
+        Returns:
+            str: A string in the format "[Square] (id) x/y - size"
+        """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
