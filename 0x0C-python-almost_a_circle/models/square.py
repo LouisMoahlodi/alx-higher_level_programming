61423af4a954f5aa4e91e2d5f463595b7fb6b575
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-08-09 13:56:56 +0200

    Correction on sqaure

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -39,15 +39,15 @@ class Square(Rectangle):
     @property
     def size(self):
         return self.width
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError('width must be > 0')
-        self.__width = value
-        self.__height = value
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """
