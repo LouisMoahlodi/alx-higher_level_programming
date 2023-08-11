5d772ac65e01b7c36d9601d2f88db857581783fc
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-08-11 07:56:54 +0200

    Updated base file to write JSON str rep

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,3 +22,23 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionairies)
+        
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Saves a list of objects to a JSON file"""
+        if list_objs is None:
+            list_objs =[]
+
+        """ Get the name of the class (cls) and create the filename """
+        class_name = cls.__name__
+        filename = f"{class_name}.json"
+
+        """ Convert the list of object to a list of dictionaires"""
+        list_dicts = [obj.to_dictionary() for obj in list_objs]
+
+        """ Convert the list of dictionaries to JSON string"""
+        json_string = cls.to_json_string(list_dicts)
+
+        """ Write the JSON string to te file"""
+        with open(filename,"w") as file:
+            file.write(json_string)
