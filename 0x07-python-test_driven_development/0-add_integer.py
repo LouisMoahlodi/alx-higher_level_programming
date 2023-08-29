8f4f881df3580918fefd1add908b4f606a4913f5
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-08-29 06:03:23 +0200

    Re-added ALX Defer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/pyhton3
+"""
+
+This module is composed of a function called 'add_integer' that takes two arguments, 'a' and 'b', and returns their sum.
+"""
+
+def add_integer(a, b=98):
+    """
+    Args:
+      a (int or float): First argument to be added.
+      b (int or float, optional): Second argument to be added. Defaults to 98.
+
+    Returns:
+     int: The sum of 'a' and 'b' as an integer.
+
+    Raises:
+        TypeError: If either of the arguments is not an integer or a float.
+    """
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+
+    return (a + b)
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Defines a rectangle"""
+
+
+class Rectangle:
+    """Represents a rectangle"""
+    def __init__(self, width=0, height=0):
+        """ Intializing the class
+
+        Args:
+            width: represents the width of the rectangle
+            height: represents the height of the rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieves the width
+
+        Raises:
+            TypeError: if width is not an int
+            ValueError: if width is less than 0
+        """
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+
+        if not type(value) is int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieves the height of the rectangle
+
+        Raises:
+            TypeError: if height is not an int
+            ValueError: if height is less o
+        """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+
+        if not type(value) is int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
