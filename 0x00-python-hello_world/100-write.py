632d5fce161af02b1ae92abde34237e3292fae4b
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-01-17 07:15:18 +0200

    Added task 11

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+
+hello_write = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+
+sys.stderr.write(hello_write + '\n')
+
+sys.exit(1)
--- quiz.py
+++ quiz.py
@@ -1,13 +1,2 @@
-class User:
-    id = 89
-    name = "no name"
-    __password = None
-
-    def __init__(self, new_name=None):
-        self.is_new = True
-        if new_name is not None:
-            self.name = new_name
-
-
-u = User()
-print (u.name)
+a = "Python is cool"
+print(a[:6])
