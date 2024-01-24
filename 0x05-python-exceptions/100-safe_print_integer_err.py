b5f9d4b071ab29504d625944ed9c9d12f2bda357
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-01-24 21:59:21 +0200

    Minor fix on task 100 file

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,14 +2,8 @@
 def safe_print_integer_err(value):
     import sys
     try:
-        # Try to convert the value to an integer
-        int_value = int(value)
-
         # IF successful, print the int followed by a new line
-        print("{:d}".format(int_value))
-
-        # Return True indicating successful printing
-        return True
+        print("{:d}".format(value))
 
     except Exception as e:
         # IF there is an exception, prin the error msg to stderr
@@ -17,3 +11,8 @@ def safe_print_integer_err(value):
 
         # Return False to show failure
         return False
+    
+    else:
+        # Return True indicating successful printing
+        return True
+    
