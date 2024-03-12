7082190fbb096e7326b894e7203f20dece3f3174
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-03-12 23:53:19 +0200

    syntax fix on task 4 file

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- This SQL command creates a table called firt_table
-CREATE TABLE IF NOT EXISTS firt_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS 'firt_table' ('id' INT, 'name' VARCHAR(256));
