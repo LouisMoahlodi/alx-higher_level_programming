f1e26b390165f3fdc88503e3b8b85ffc3fca3458
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-03-14 23:23:33 +0200

    Added task 7 file

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,7 @@
 -- This SQL command creates a new database and table in the created database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; 
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-    id INT AUTO_INCREMENT PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT, FOREIGN KEY (state_id) REFERENCES states(id),
     name VARCHAR(256)
     );
