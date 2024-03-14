34250516432c58038d153e4488153ba3cd0b7367
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-03-14 20:22:04 +0200

    modify 1 on taks 1 file

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,12 @@
+-- MySQL user details
+SET @username = 'user_0d_1';
+SET @password = 'user_0d_1_pwd';
+
 -- Creating a new user with all privilages 
-CREATE USER IF NOT EXISTS  'user_0d_1'@'localhost' IDENNTIFIED BY user_0d_1_pwd;
+CREATE USER IF NOT EXISTS  @username@'localhost' IDENNTIFIED BY @password;
 
 -- Grant all privilages to the user
-GRANT ALL  PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'  WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO @username@'localhost'  WITH GRANT OPTION;
 
 -- Fkush all privileges to apply changes
 FLUSH PRIVILEGES;
