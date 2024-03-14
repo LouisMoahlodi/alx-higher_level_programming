76789f7ec35f1a6aae7d5beb9d20c19a38105cd9
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-03-14 20:31:26 +0200

    modify 2 on task 1 file

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Creating a new user with all privilages 
-CREATE USER IF NOT EXISTS  'user_0d_1'@'localhost' IDENNTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS  'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant all privilages to the user
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'  WITH GRANT OPTION;
 -- Fkush all privileges to apply changes
