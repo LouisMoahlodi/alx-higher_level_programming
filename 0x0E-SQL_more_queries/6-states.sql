40bb4f9748899778870b75e817c72c23ecb46bbf
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-03-14 22:57:21 +0200

    modify 2 on task 6 file

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- This SQL command creates a new database in MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
 -- This SQl command creates the table in the created database
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'states' (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.'states' (
     'id' INT(1) AUTO_INCREMENT  PRIMARY KEY, 
     'name' VARCHAR(256)
 );
