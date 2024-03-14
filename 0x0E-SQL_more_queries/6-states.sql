8cd8b37e4ce792aa88faf3ec30017a782edcb15a
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-03-14 23:06:43 +0200

    modify 3 on task 6 file

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,2 @@
--- This SQL command creates a new database in MySQL server
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
--- This SQl command creates the table in the created database
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.'states' (
-    'id' INT(1) AUTO_INCREMENT  PRIMARY KEY, 
-    'name' VARCHAR(256)
-);
+-- This SQL command creates a new database and table in the created database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256));
