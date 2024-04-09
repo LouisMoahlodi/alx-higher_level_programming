86364ac8cca4411459200ca26663d1fce445abc6
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-04-09 04:29:31 +0200

    modify 2 on task 2 file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const arguments = process.argv.slice(2);
+const args = process.argv.slice(2);
 
 // Check the number of arguments
-if (arguments.length === 0){
+if (args.length === 0){
     console.log("No agrument");
-} else if (arguments.length === 1){
+} else if (args.length === 1){
     console.log("Argument found");
 } else {
     console.log("Arguments found")
