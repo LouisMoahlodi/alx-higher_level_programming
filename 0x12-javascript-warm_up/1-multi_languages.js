631de4574d3c54dc63c593d74b7781d4ade462ab
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-04-09 03:51:38 +0200

    modify 2 task 1 file

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
-const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+/* const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let message of messages) {
   console.log(message);
-}
+} */
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
